22390cc8c48c3e0578d218f12d3d77d1ad1d75ec
Rodrigo Alejandro Perez <rodrigo.alejandro.perez.104@gmail.com>
2023-09-19 12:08:55 -0300

    funca

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,31 +1,34 @@
-import Image from "next/image";
+import axios from "axios";
 
 const fetchProducts = async () => {
-    const res = await fetch(
+  try {
+    const res = await axios.get(
       "https://virtual-e-commerce-k1ks.onrender.com/Products"
     );
-    const data = await res.json();
-    return data;
-  };
-  
-  const Card = async () => {
-    const products = await fetchProducts();
-    return (
-      <ul className="bg-gray-500">
-        {products.map((product) => (
-           <li key={product._id}>
-           <div>
-             <h1>{product.name}</h1>
-             <p>{product.type}</p>
-             <p>{product.description}</p>
-             <h1>{product.Precio}</h1>
-           </div>
-           <img src={product.img} alt={product.name} />
-         </li>
-        ))}
-      </ul>
-    );
-  };
-  
-  export default Card;
-  
+    return res.data; // Axios ya ha procesado la respuesta JSON
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    return []; // Maneja el error de manera apropiada
+  }
+};
+
+const Card = async () => {
+  const products = await fetchProducts();
+  return (
+    <ul className="bg-gray-500">
+      {products.map((product) => (
+        <li key={product._id}>
+          <div>
+            <h1>{product.name}</h1>
+            <p>{product.type}</p>
+            <p>{product.description}</p>
+            <h1>{product.Precio}</h1>
+          </div>
+          <img src={product.img} alt={product.name} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Card;
