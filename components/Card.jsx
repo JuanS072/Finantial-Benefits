fa95490cef83919e885bebf129f22087ed9f806b
JuanS072 <juancarlossequeira07@gmail.com>
2023-09-19 12:11:08 -0300

    Card css

--- components/Card.jsx
+++ components/Card.jsx
@@ -15,19 +15,21 @@ const fetchProducts = async () => {
 const Card = async () => {
   const products = await fetchProducts();
   return (
-    <ul className="bg-gray-500">
+    <div className="">
+    <ul className="flex flex-row mx-3">
       {products.map((product) => (
-        <li key={product._id}>
-          <div>
-            <h1>{product.name}</h1>
-            <p>{product.type}</p>
-            <p>{product.description}</p>
-            <h1>{product.Precio}</h1>
-          </div>
-          <img src={product.img} alt={product.name} />
-        </li>
+         <li key={product._id} className="mx-1 my-5">
+         <div className="bg-white px-6 py-5 border-black border-x-2 border-y-2 border-opacity-10">
+           <img src={product.img} alt={product.name} className="w-72 h-52 border-black border-x-2 border-y-2 border-opacity-10" />
+           <h1>{product.name}</h1>
+           <p>{product.type}</p>
+           <p>{product.description}</p>
+           <h1>{product.Precio}</h1>
+         </div>
+       </li>
       ))}
     </ul>
+    </div>
   );
 };
 
